f097070957c6bdb6cb2f8c840b3e69489975a23b
crcrewso <6089951+crcrewso@users.noreply.github.com>
2020-06-10 14:16:49 -0600

    attempt at better display untested

--- cat-script-LatShift.py
+++ cat-script-LatShift.py
@@ -184,12 +184,53 @@ elif "new" in var.lower():
     import vtk
     from PyQt5 import Qt
     import vtkplotter as vtkp
-    volume = vtkp.load(pathout)
-    vp = vtkp.show(volume, interactive=0)
-    vp.interactor.Render()
-    vp.interactor.Start()
+    from vtk.qt.QVTKRenderWindowInteractor import QVTKRenderWindowInteractor
+    vol = vtkp.load(pathout)
+    if __name__ == "__main__":
+        app = Qt.QApplication(sys.argv)
+        window = MainWindow()
+        sys.exit(app.exec_())
+
 
 sys.exit()
 
 
 
+class MainWindow(Qt.QMainWindow):
+
+    def __init__(self, parent=None):
+        Qt.QMainWindow.__init__(self, parent)
+
+        self.frame = Qt.QFrame()
+
+        self.vl = Qt.QVBoxLayout()
+        self.vtkWidget = QVTKRenderWindowInteractor(self.frame)
+        self.vl.addWidget(self.vtkWidget)
+
+        self.ren = vtk.vtkRenderer()
+        self.vtkWidget.GetRenderWindow().AddRenderer(self.ren)
+        self.iren = self.vtkWidget.GetRenderWindow().GetInteractor()
+
+        # Create source
+        source = vtk.vtkSphereSource()
+        source.SetCenter(0, 0, 0)
+        source.SetRadius(1000.0)
+
+        # Create a mapper
+        mapper = vtk.vtkPolyDataMapper()
+        mapper.SetInputConnection(source.GetOutputPort())
+
+        # Create an actor
+        actor = vtk.vtkActor()
+        actor.SetMapper(mapper)
+
+        self.ren.AddActor(actor)
+        self.ren.AddVolume(vol)
+
+        self.ren.ResetCamera()
+
+        self.frame.setLayout(self.vl)
+        self.setCentralWidget(self.frame)
+
+        self.show()
+        self.iren.Initialize()
